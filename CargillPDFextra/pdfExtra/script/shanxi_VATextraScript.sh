0d2495e629817de05f5bafb4efc6a3dd48337484
jojo <wangjiaojiao640801@163.com>
2019-07-04 10:42:23 +0800

    update 陕西和泰安

---
+++ CargillPDFextra/pdfExtra/script/shanxi_VATextraScript.sh
@@ -0,0 +1,1 @@
+java -Dfile.encoding=gbk  -jar  tabula-1.0.2.jar  -a 144.473,33.748,647.248,583.379 -p $pageNo -l -o temp/$csv_path $filePath
---
+++ CargillPDFextra/pdfExtra/script/taian_VATextraScript.sh
@@ -0,0 +1,1 @@
+java -Dfile.encoding=gbk  -jar  tabula-1.0.2.jar  -a 166.786,27.798,695.592,590.073 -p $pageNo -l -o temp/$csv_path $filePath
